9a5afdf18cd7a1f1eb42ab76782322a6ee8c401c
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-05-05 17:19:25 +0500

    added firebase auth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import { useAuth } from '../lib/auth'
 export default function Home() {
+  const auth = useAuth()
   return (
     <div className={styles.container}>
       <Head>
@@ -20,35 +21,10 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+{!auth?.user &&  <button onClick={(e) => auth.signinWithGitHub()}>Sign In!</button>}
+{auth?.user && <button onClick={(e) => auth.signout()}>Sign Out!</button>}
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+         {auth?.user?.email}
         </div>
       </main>
 
